c282c3c066081a09a9cba9704eb42fc462362d12
Ahmed Mohamad Youssef <amh247408@gmail.com>
2025-11-16 20:26:52 +0200

    Final Structure

--- hr-management/src/payroll-config/dto/create-deduction.dto.ts
+++
@@ -1,8 +0,0 @@
-import { DeductionCalcType } from '../models/deduction.schema';
-
-export class CreateDeductionDto {
-  code: string;
-  name: string;
-  calcType: DeductionCalcType;
-  value: number;
-}
