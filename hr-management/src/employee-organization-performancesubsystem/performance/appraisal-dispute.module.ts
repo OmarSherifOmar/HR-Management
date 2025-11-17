99c0188eac0420da6109e257a09d4f30e9ccf197
Ahmed Mourad <89593254+mourad2213@users.noreply.github.com>
2025-11-17 18:55:04 +0200

    Add imports for FinalAppraisal, Employee, and Notification modules

--- hr-management/src/employee-organization-performancesubsystem/performance/appraisal-dispute.module.ts
+++ hr-management/src/employee-organization-performancesubsystem/performance/appraisal-dispute.module.ts
@@ -3,6 +3,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppraisalDispute, AppraisalDisputeSchema } from './models/appraisal-dispute.schema';
 import { AppraisalDisputeService } from './appraisal-dispute.service';
 import { AppraisalDisputeController } from './appraisal-dispute.controller';
+import { FinalAppraisalModule } from '../final-appraisal.module';
+import { EmployeeModule } from 'src/employee-organization-performancesubsystem/employee/employee.module';
+import { NotificationModule } from 'src/employee-organization-performancesubsystem/organization/notification.module';
 
 @Module({
   imports: [
