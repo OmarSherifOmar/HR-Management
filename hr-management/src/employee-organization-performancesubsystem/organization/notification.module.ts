334e15b6ba17a6ccd84fb8ac8a745ed21fc299d6
Ahmed Mourad <89593254+mourad2213@users.noreply.github.com>
2025-11-17 18:43:06 +0200

    Add EmployeeModule and DepartmentModule imports

--- hr-management/src/employee-organization-performancesubsystem/organization/notification.module.ts
+++ hr-management/src/employee-organization-performancesubsystem/organization/notification.module.ts
@@ -3,7 +3,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Notification, NotificationSchema } from './models/notification.schema';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
-
+import { EmployeeModule } from '../employee/employee.module';
+import { DepartmentModule } from './department.module';
 @Module({
   imports: [
     MongooseModule.forFeature([
