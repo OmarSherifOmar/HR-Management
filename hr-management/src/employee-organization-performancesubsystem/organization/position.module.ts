96ad302211321a57a91d0d8c596095a105d68f98
Ahmed Mourad <89593254+mourad2213@users.noreply.github.com>
2025-11-17 18:47:28 +0200

    Import DepartmentModule in position.module.ts

--- hr-management/src/employee-organization-performancesubsystem/organization/position.module.ts
+++ hr-management/src/employee-organization-performancesubsystem/organization/position.module.ts
@@ -3,6 +3,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Position, PositionSchema } from './models/position.schema';
 import { EmployeeModule } from '../employee/employee.module';
 import { Department } from './models/department.schema';
+import { EmployeeModule } from '../employee/employee.module';
+import { DepartmentModule } from './department.module';
+
 @Module({
   imports: [
     MongooseModule.forFeature([
