f2ec17735962bb2366cb8b86d814bf83ca325dcd
XdahabiX <omardahabi.ba@gmail.com>
2025-11-17 19:42:58 +0200

    fix

--- hr-management/src/payroll/dto/update-deduction.dto.ts
+++
@@ -1,8 +0,0 @@
-import type { DeductionCalcType } from './create-deduction.dto';
-
-export class UpdateDeductionDto {
-  code?: string;
-  name?: string;
-  calcType?: DeductionCalcType;
-  value?: number;
-}
