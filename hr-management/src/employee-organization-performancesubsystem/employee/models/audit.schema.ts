09cb917fa9307bbb4af6764df8ce2df59e06fe09
Ahmed Mourad <89593254+mourad2213@users.noreply.github.com>
2025-11-17 18:58:08 +0200

    Update audit.schema.ts

--- hr-management/src/employee-organization-performancesubsystem/employee/models/audit.schema.ts
+++ hr-management/src/employee-organization-performancesubsystem/employee/models/audit.schema.ts
@@ -1,6 +1,9 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 import { Employee } from './employee.schema';
+import { Employee } from './employee.schema';
+import { Notification } from 'src/employee-organization-performancesubsystem/organization/models/notification.schema';
+
 export type AuditLogDocument = AuditLog & Document;
 
 @Schema({ timestamps: true, versionKey: false })
