a91c00d0bf3025b797fc44625af75df2eee70c9d
Omar Hany <omarhany@outlook.com>
2025-11-17 17:55:43 +0200

    added missing files

--- hr-management/src/time-managment-subsystem/modules/shift-module.module.ts
+++ hr-management/src/time-managment-subsystem/modules/shift-module.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ShiftSchema } from '../models/shift.schema';
 
-@Module({})
+@Module({
+	imports: [
+		MongooseModule.forFeature([
+			{ name: 'Shift', schema: ShiftSchema },
+		]),
+	],
+})
 export class ShiftModuleModule {}
