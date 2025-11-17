1b261396effa2f64aec93e2d71a9e70021803527
Ahmed Mourad <89593254+mourad2213@users.noreply.github.com>
2025-11-17 18:26:43 +0200

    Add PayGrade import to position.schema.ts

--- hr-management/src/employee-organization-performancesubsystem/organization/models/position.schema.ts
+++ hr-management/src/employee-organization-performancesubsystem/organization/models/position.schema.ts
@@ -2,7 +2,7 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 import { Employee } from './employee.schema';
 import { Department } from 'src/employee-organization-performancesubsystem/organization/models/department.schema';
-
+import {PayGrade} from './paygrade.schema'
 
 export type PositionDocument = Position & Document;
 
