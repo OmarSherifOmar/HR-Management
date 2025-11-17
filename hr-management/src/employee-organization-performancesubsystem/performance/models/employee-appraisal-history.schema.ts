33b07730b06d95134fe65dc6f43c2fc6b6ba291c
Ahmed Mourad <89593254+mourad2213@users.noreply.github.com>
2025-11-17 18:33:59 +0200

    Import FinalAppraisalRecordSchema in employee appraisal

--- hr-management/src/employee-organization-performancesubsystem/performance/models/employee-appraisal-history.schema.ts
+++ hr-management/src/employee-organization-performancesubsystem/performance/models/employee-appraisal-history.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
+import { FinalAppraisalRecordSchema } from './final-appraisal-record.schema';
 
 export type EmployeeAppraisalHistoryDocument = EmployeeAppraisalHistory & Document;
 
