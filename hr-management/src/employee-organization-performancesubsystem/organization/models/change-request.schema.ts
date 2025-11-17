cefbfa561145c81101602c2d4e436a6c0bb1fbd3
Ahmed Mourad <89593254+mourad2213@users.noreply.github.com>
2025-11-17 18:20:44 +0200

    Import Employee schema in change-request.schema.ts

--- hr-management/src/employee-organization-performancesubsystem/organization/models/change-request.schema.ts
+++ hr-management/src/employee-organization-performancesubsystem/organization/models/change-request.schema.ts
@@ -1,5 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
+import { Employee } from './employee.schema';
+
 
 export type ChangeRequestDocument = ChangeRequest & Document;
 
