0eba9d3a574f4ae10de5e4f9d06ed70b4d477596
Ahmed Mourad <89593254+mourad2213@users.noreply.github.com>
2025-11-17 18:17:34 +0200

    Import Employee model in audit.schema.ts

--- hr-management/src/employee-organization-performancesubsystem/employee/models/audit.schema.ts
+++ hr-management/src/employee-organization-performancesubsystem/employee/models/audit.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
-
+import { Employee } from './employee.schema';
 export type AuditLogDocument = AuditLog & Document;
 
 @Schema({ timestamps: true, versionKey: false })
