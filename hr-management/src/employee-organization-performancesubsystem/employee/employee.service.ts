a7c0c39a78d213a6620cecd42938e97cfa0a9397
Omar <omarsherifmohammedomar@gmail.com>
2025-11-27 23:38:05 +0200

    Merge pull request #5 from mourad2213/main
    merge of main into employee-org-performance

--- hr-management/src/employee-organization-performancesubsystem/employee/employee.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable, NotFoundException, ForbiddenException, BadRequestException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { isFieldGoverned } from './models/employee.schema';
-import { Employee } from './models/employee.schema';
-import { AuditLogDocument } from './models/audit.schema';
-
