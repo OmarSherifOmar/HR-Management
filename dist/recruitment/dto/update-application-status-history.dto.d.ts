2b2975c600d137df9f77b73cc70551cf9b76cafc
Faroouuk <arahman.farouk@icloud.com>
2025-11-28 14:48:06 +0200

    Import HR-Management mother repo

---
+++ dist/recruitment/dto/update-application-status-history.dto.d.ts
@@ -0,0 +1,5 @@
+import { CreateApplicationStatusHistoryDto } from './create-application-status-history.dto';
+declare const UpdateApplicationStatusHistoryDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateApplicationStatusHistoryDto>>;
+export declare class UpdateApplicationStatusHistoryDto extends UpdateApplicationStatusHistoryDto_base {
+}
+export {};
