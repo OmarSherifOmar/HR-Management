98518a659a07e7ba54db60ab3a3db648fc4c45c7
Faroouuk <arahman.farouk@icloud.com>
2025-11-29 11:27:36 +0200

    modified:   .DS_Store 	new file:   backend/src/recruitment/controllers/application-status-history.controller.ts 	new file:   backend/src/recruitment/dtos/create-application-status-history.dto.ts 	new file:   backend/src/recruitment/dtos/update-application-status-history.dto.ts 	modified:   backend/src/recruitment/recruitment.module.ts 	new file:   backend/src/recruitment/services/application-status-history.service.ts

--- backend/src/recruitment/recruitment.module.ts
+++ backend/src/recruitment/recruitment.module.ts
@@ -1,22 +1,29 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { RecruitmentController } from './recruitment.controller';
 import { RecruitmentService } from './recruitment.service';
-import { MongooseModule } from '@nestjs/mongoose';
+
+// Models
 import { JobTemplate, JobTemplateSchema } from './models/job-template.schema';
-import { JobRequisition,JobRequisitionSchema } from './models/job-requisition.schema';
-import { Application,ApplicationSchema } from './models/application.schema';
-import { ApplicationStatusHistory,ApplicationStatusHistorySchema } from './models/application-history.schema';
-import { Interview,InterviewSchema } from './models/interview.schema';
-import { AssessmentResult,AssessmentResultSchema } from './models/assessment-result.schema';
-import { Referral,ReferralSchema } from './models/referral.schema';
-import { Offer,OfferSchema } from './models/offer.schema';
-import { Contract,ContractSchema } from './models/contract.schema';
-import { Document,DocumentSchema } from './models/document.schema';
-import { TerminationRequest,TerminationRequestSchema } from './models/termination-request.schema';
-import { ClearanceChecklist,ClearanceChecklistSchema } from './models/clearance-checklist.schema';
+import { JobRequisition, JobRequisitionSchema } from './models/job-requisition.schema';
+import { Application, ApplicationSchema } from './models/application.schema';
+import { ApplicationStatusHistory, ApplicationStatusHistorySchema } from './models/application-history.schema';
+import { Interview, InterviewSchema } from './models/interview.schema';
+import { AssessmentResult, AssessmentResultSchema } from './models/assessment-result.schema';
+import { Referral, ReferralSchema } from './models/referral.schema';
+import { Offer, OfferSchema } from './models/offer.schema';
+import { Contract, ContractSchema } from './models/contract.schema';
+import { Document, DocumentSchema } from './models/document.schema';
+import { TerminationRequest, TerminationRequestSchema } from './models/termination-request.schema';
+import { ClearanceChecklist, ClearanceChecklistSchema } from './models/clearance-checklist.schema';
 import { EmployeeProfileModule } from '../employee-profile/employee-profile.module';
+import { ApplicationStatusHistoryService } from './services/application-status-history.service';
+import { ApplicationStatusHistoryController } from './controllers/application-status-history.controller';
+
 @Module({
-  imports:[MongooseModule.forFeature([
+  imports: [
+    MongooseModule.forFeature([
       { name: JobTemplate.name, schema: JobTemplateSchema },
       { name: JobRequisition.name, schema: JobRequisitionSchema },
       { name: Application.name, schema: ApplicationSchema },
@@ -29,11 +36,24 @@ import { EmployeeProfileModule } from '../employee-profile/employee-profile.modu
       { name: Document.name, schema: DocumentSchema },
       { name: TerminationRequest.name, schema: TerminationRequestSchema },
       { name: ClearanceChecklist.name, schema: ClearanceChecklistSchema },
-    ]),EmployeeProfileModule
-  ],
-  controllers: [RecruitmentController],
-  providers: [RecruitmentService],
-  exports:[RecruitmentService]
+    ]),
 
+    EmployeeProfileModule,
+  ],
+
+  controllers: [
+    RecruitmentController,
+    ApplicationStatusHistoryController,
+  ],
+
+  providers: [
+    RecruitmentService,
+    ApplicationStatusHistoryService,
+  ],
+
+  exports: [
+    RecruitmentService,
+    ApplicationStatusHistoryService,
+  ],
 })
 export class RecruitmentModule {}
