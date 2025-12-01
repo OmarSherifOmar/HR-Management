5f11aca45f8dac2e427714a15c3a9f04871773f2
Razan :( <razan.ghanem605@gmail.com>
2025-12-01 22:54:42 +0200

    Add ShiftType schema to shift.module

--- backend/src/time-management/modules/shift.module.ts
+++ backend/src/time-management/modules/shift.module.ts
@@ -3,10 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ShiftController } from '../controllers/shift.controller';
 import { ShiftService } from '../services/shift.service';
 import { Shift, ShiftSchema } from '../models/shift.schema';
+import { ShiftType, ShiftTypeSchema } from '../models/shift-type.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Shift.name, schema: ShiftSchema }])
+    MongooseModule.forFeature([
+      { name: Shift.name, schema: ShiftSchema },
+      { name: ShiftType.name, schema: ShiftTypeSchema }
+    ])
   ],
   controllers: [ShiftController],
   providers: [ShiftService],
